155d9e402e9bfe16eeaaca56cc5bd3eeae90182d
Noel Kleen <noelhkleen@gmail.com>
2026-04-27 10:55:24 +0200

    feat(htmx): vendor htmx 2.x core + sse extension

--- tests/test_static.py
+++ tests/test_static.py
@@ -4,3 +4,16 @@ async def test_tokens_css_is_served(client):
     assert response.status_code == 200
     assert "--main-color: #b5533a" in response.text
     assert response.headers["content-type"].startswith("text/css")
+
+
+async def test_htmx_is_served(client):
+    response = await client.get("/static/htmx.min.js")
+    assert response.status_code == 200
+    assert response.headers["content-type"].startswith(("application/javascript", "text/javascript"))
+    assert len(response.content) > 30_000
+
+
+async def test_htmx_sse_extension_is_served(client):
+    response = await client.get("/static/htmx-ext-sse.js")
+    assert response.status_code == 200
+    assert response.headers["content-type"].startswith(("application/javascript", "text/javascript"))
